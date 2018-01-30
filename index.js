4d4017e360670ff3ca9863c2164ac2415be91fd1
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-30 20:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese(foods) {
   for (var i = 0; i < foods.length; i++) {
-    if (foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert') {
+    if (foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert' || foods[i] === 'swiss') {
       return foods[i];
     }
   }
